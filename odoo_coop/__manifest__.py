83c98a2e165f4e28ecf0b642a09a811978ffbe22
Sheraya Smith <sheraya@sincerelyhers.com>
2021-04-05 19:36:43 +0000

    [ADD] Coop Volunteer Module

---
+++ odoo_coop/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Co-op Volunteering',
+    'summary': """App to manage Co-op Volunteers and Volunteering""",
+    'description': """
+        App to manage Co-op Volunteers and Volunteering:
+        -Volunteers
+        -Volunteer hours
+        -Co-op storefront
+    """,
+    
+    'author': 'Sheraya Smith',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+        
+    ], 
+    
+    'demo': [
+        
+        
+    ],
+}
